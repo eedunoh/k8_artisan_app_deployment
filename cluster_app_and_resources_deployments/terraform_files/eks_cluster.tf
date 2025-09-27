621a9004bde8565c5d97f130b22537999d0bab5c
eedunoh <63975169+eedunoh@users.noreply.github.com>
2025-09-27 22:59:55 +0100

    changed monitoring node instance type

--- cluster_app_and_resources_deployments/terraform_files/eks_cluster.tf
+++ cluster_app_and_resources_deployments/terraform_files/eks_cluster.tf
@@ -33,7 +33,7 @@ module "eks" {
     monitoring_node = {
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
       ami_type       = "AL2023_x86_64_STANDARD"
-      instance_type = "t3.medium"
+      instance_type = "m6i.xlarge"
 
       min_size     = 1
       max_size     = 2
